835d0f6a1b905b6f54fd6ad6a34646b1cf60de02
unknown <235568@student.pwr.edu.pl>
2020-10-04 11:25:18 +0200

    Added navigation button, now user can run through all of questions and go to summary

--- frontned/src/components/quiz/Questions.js
+++ frontned/src/components/quiz/Questions.js
@@ -17,6 +17,8 @@ const Questions = ({id, setView}) => {
 
   const [quizOver, setQuizOver] = useState(false);
 
+  const [answerChecked, setAnswerChecked] = useState(false);
+
   useEffect(() => {
     quizService
     .getOne(id)
@@ -29,23 +31,66 @@ const Questions = ({id, setView}) => {
   }, [])
 
   const apply = () => {
+
+    if(selected === questions[counter].valid) {
+      setPoints(p => p+1)
+    }
+
+    const historyItem ={
+      selected: selected,
+      valid: questions[counter].valid
+    }
+
+    setAnswerChecked(true)
+
+    setHistory(h => h.concat(historyItem))
+
+    // if(counter+1 !== questions.length) {
+
+    //   setCounter(c => c+1)
+    //   setHistory(h => h.concat(historyItem))
+    //   setSelected("")
+    // } else {
+    //   setHistory(h => h.concat(historyItem))
+    //   setQuizOver(true)
+    // }
+  }
+
+  const next = () => {
     if(counter+1 !== questions.length) {
+
       setCounter(c => c+1)
-      setHistory(h => h.concat({'selected': selected, 'valid': questions[counter].valid}))
+      // setHistory(h => h.concat(historyItem))
+      setSelected("")
+      setAnswerChecked(false)
     } else {
+      // setHistory(h => h.concat(historyItem))
       setQuizOver(true)
     }
   }
 
-  const finish = () => {
-    setView('info')
+  const applyButton = () => {
+    return (
+      <IonButton onClick={apply}>
+        apply
+      </IonButton>
+    )
   }
+
+  const nextButton = () => {
+    return (
+      <IonButton onClick={next}>
+        next
+      </IonButton>
+    )
+  }
+
 
   const quiz = () => {
     return (
       <>
         <IonCardHeader>
-          <IonText>0pkt</IonText>
+          <IonText>{points}pkt</IonText>
           <IonText>
             <h4>{!loading ? questions[counter].pytanie : 'loading'}</h4>
           </IonText>
@@ -62,9 +107,7 @@ const Questions = ({id, setView}) => {
           </IonRadioGroup>
         </IonCardContent>
 
-        <IonButton onClick={apply}>
-          apply
-        </IonButton>
+        {answerChecked ? nextButton() : applyButton()}
       </>
     )
   }
@@ -73,17 +116,24 @@ const Questions = ({id, setView}) => {
     return (
       <>
         <IonCardHeader>
-          <IonText>0pkt</IonText>
+          <IonText>{points}pkt</IonText>
           <IonText>
             <h4>Summary</h4>
           </IonText>
         </IonCardHeader>
 
         <IonCardContent>
+          {history.map((h, i) => {
+            return(
+              <IonItem key={i}>
+                <IonLabel>selected{h.selected}</IonLabel>
+                <IonLabel>valid{h.valid}</IonLabel>
+              </IonItem>
+            )
+          })}
 
         </IonCardContent>
-
-        <IonButton routerLink={"/quizchoice"}onClick={finish}>
+        <IonButton onClick={() => setView('info')}>
           Go back to quizes
         </IonButton>
       </>
@@ -93,7 +143,6 @@ const Questions = ({id, setView}) => {
   return(
     <IonCard>
       {quizOver ? summary() : quiz()}
-
     </IonCard>
   )
 }
