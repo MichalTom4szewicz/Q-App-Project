3f4ba1d97f1c723e1bffb8b40a599a3a069939c6
unknown <235568@student.pwr.edu.pl>
2020-10-18 14:39:54 +0200

    Shuffler class was braking whole app (in production) so it got commented out
    functionality of shuffler will be moved out of class later

--- frontned/src/components/quiz/Shuffler.js
+++ frontned/src/components/quiz/Shuffler.js
@@ -1,17 +1,17 @@
 
-class Shuffler {
-  constructor() {}
+// class Shuffler {
+//   constructor() {}
 
-  shuffle(array) {
-    let j, x, i;
-    for (i = array.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = array[i];
-        array[i] = array[j];
-        array[j] = x;
-    }
-    return array;
-  }
-}
+//   shuffle(array) {
+//     let j, x, i;
+//     for (i = array.length - 1; i > 0; i--) {
+//         j = Math.floor(Math.random() * (i + 1));
+//         x = array[i];
+//         array[i] = array[j];
+//         array[j] = x;
+//     }
+//     return array;
+//   }
+// }
 
-module.exports = Shuffler;
+// module.exports = Shuffler;
