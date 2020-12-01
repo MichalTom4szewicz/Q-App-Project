bb1f698c2d648821a80ee10636738d8e98c805a3
Michal Tomaszewicz <235568@student.pwr.edu.pl>
2020-12-01 13:44:06 +0100

    w3 websocket work

--- frontned/src/pages/FriendPanel.js
+++ frontned/src/pages/FriendPanel.js
@@ -19,16 +19,18 @@ import {IonList,
   IonRefresherContent,
   IonMenuButton,
   IonButtons,
-  IonAlert
+  IonAlert,
+  IonTextarea
 } from '@ionic/react';
 
 import React, {useState, useEffect} from 'react';
 
 import usersService from '../services/users'
 import User from '../components/friend/User'
-// import quizPreviewService from '../services/quizPreviews'
-// import QuizPreview from '../components/userpanel/QuizPreview';
 
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
 
 const FriendPanel = (props) => {
 
@@ -40,11 +42,24 @@ const FriendPanel = (props) => {
 
   const [showPreview, setShowPreview] = useState(false);
 
+  const [messages, setMessages] = useState([]);
+
+  const [text, setText] = useState('');
+
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedUser')
     const user = JSON.parse(loggedUserJSON)
     setFriends(user.friends)
+
+    client.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      console.log('got reply! ', dataFromServer);
+      if (dataFromServer.type === "message") {
+        setMessages(m => m.concat(dataFromServer.msg))
+      }
+    };
+
   }, []);
 
   const fetchFriend = user => {
@@ -77,6 +92,21 @@ const FriendPanel = (props) => {
       </IonHeader>
 
       <IonContent>
+
+        <IonTextarea placeholder="here..." value={text} onIonChange={e => setText(e.detail.value)}></IonTextarea>
+        {text}
+
+        <IonButton onClick={() => {
+          const loggedUserJSON = window.localStorage.getItem('loggedUser')
+          const user = JSON.parse(loggedUserJSON)
+
+          client.send(JSON.stringify({
+            type: "message",
+            msg: text,
+            user: user.username
+          }));
+        }}>click</IonButton>
+
         <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
             <IonRefresherContent></IonRefresherContent>
         </IonRefresher>
@@ -91,11 +121,17 @@ const FriendPanel = (props) => {
                       </IonItem>
                     )
                   })}
-                </> : <IonText>loading</IonText>
+                </> : <IonText>loadin</IonText>
               }
           </> :
           <User fsetter={setFriends} setter={setChoosenFriend} user={choosenFriend} />
         }
+
+        {messages.map(m => {
+          return (
+          <p>{m}</p>
+          )
+        })}
 
     </IonContent>
 
